fc28b358f046129a6b5f171bea00fe349b70c362
syedali <infosyedalimoiz99@gmail.com>
2024-11-12 15:14:43 +0500

    add HashRouter  and set layout

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -3,23 +3,23 @@ import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav className='navBar justify-content-between justify-content-lg-start z_inf'>
-      <Link className="slogan" to={'/'}>Byte & Beyond</Link>
+    <nav className='navBar row z_inf'>
+      <Link className="slogan col-md-2 col-5 d-flex justify-content-center" to={'/'}>Byte&Beyond</Link>
 
-        <ul className="links m-0">
+      <div className="d-flex justify-content-md-between justify-content-evenly col-md-9 col-7">
+        <div className="links m-0">
           <li>
-            <NavLink className="link" to={'/'}>Home</NavLink>
+            <NavLink className="link d-none-768" to={'/'}>Home</NavLink>
           </li>
           <li>
             <NavLink className="link" to={'/blogs'}>Blogs</NavLink>
           </li>
-          <li>
-            <NavLink className="link" to={'/admin'}>admin</NavLink>
-          </li>
-          <li>
-            <NavLink className="link" to={'/author'}>author</NavLink>
-          </li>
-        </ul>
+        </div>
+        <div className="links m-0">
+          
+            <NavLink className="link" to={'/login'}><i class="fas fa-user-circle"></i></NavLink>
+        </div>
+      </div>
 
     </nav>
   )
